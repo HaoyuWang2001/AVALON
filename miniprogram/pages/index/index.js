c1e8b5ce1bbb5856b8c5edc0899822dfb680d833
Haoyu <haoyu.wang141@outlook.com>
2026-09-24 20:04:23 +0800

    Revert "refactor(home): 主页改版——品牌顶栏/强调式用户卡/我的数据分段卡 + 主题色适配"
    This reverts commit c00e7b6ed5e0d82ba1358bcf7911ab6e52f65913.

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -35,7 +35,6 @@ Page({
     goodWinRate: '',
     evilWinRate: '',
     roleStats: [],
-    dataTab: 'role',
     champions: [],
     podium: [],
     championModal: { show: false, openId: '', nickName: '', avatarUrl: '', isFriend: false, uniqueId: '', isSelf: false, loading: false, stats: null, roles: [] }
@@ -478,12 +477,6 @@ Page({
   },
 
   noop() {},
-
-  // 我的数据分段切换（角色胜率 / 历史对局）
-  switchDataTab(e) {
-    const tab = e.currentTarget.dataset.tab;
-    if (tab) this.setData({ dataTab: tab });
-  },
 
   showInfoModal() {
     this.setData({ showInfo: true });
